1afcd8409cf21825c3c8d8398a3b3878b3af71fd
unknown <anasabdulmajeed08@gmail.com>
2020-05-27 02:29:32 +0300

    almost there

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,25 @@
+const currentTask = process.env.npm_lifecycle_event
 const path = require("path"),
-  postCSSPlugins = [
-    require("postcss-import"),
-    require("postcss-mixins"),
-    require("postcss-simple-vars"),
-    require("postcss-nested"),
-    require("postcss-hexrgba"),
-    require("postcss-color-function"),
-    require("autoprefixer")
-  ]
+  postCSSPlugins = [require("postcss-import"), require("postcss-mixins"), require("postcss-simple-vars"), require("postcss-nested"), require("postcss-hexrgba"), require("postcss-color-function"), require("autoprefixer")]
 
-module.exports = {
+let config = {
   entry: "./app/assets/scripts/App.js",
-  output: {
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader?url=false", { loader: "postcss-loader", options: { plugins: postCSSPlugins } }]
+      }
+    ]
+  }
+}
+
+if (currentTask == "dev") {
+  config.output = {
     filename: "bundled.js",
     path: path.resolve(__dirname, "app")
-  },
-  devServer: {
+  }
+  config.devServer = {
     before: function (app, server) {
       server._watch("./app/**/*.html")
     },
@@ -23,18 +27,46 @@ module.exports = {
     hot: true,
     port: 3000,
     host: "0.0.0.0"
-  },
-  mode: "development",
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: [
-          "style-loader",
-          "css-loader?url=false",
-          { loader: "postcss-loader", options: { plugins: postCSSPlugins } }
-        ]
-      }
-    ]
   }
+  config.mode = "development"
 }
+
+if (currentTask == "build") {
+  config.output = {
+    filename: "bundled.js",
+    path: path.resolve(__dirname, "dist")
+  }
+  config.mode = "production"
+}
+
+// let deleteMeLater = {
+//   entry: "./app/assets/scripts/App.js",
+//   output: {
+//     filename: "bundled.js",
+//     path: path.resolve(__dirname, "app")
+//   },
+//   devServer: {
+//     before: function (app, server) {
+//       server._watch("./app/**/*.html")
+//     },
+//     contentBase: path.join(__dirname, "app"),
+//     hot: true,
+//     port: 3000,
+//     host: "0.0.0.0"
+//   },
+//   mode: "development",
+//   module: {
+//     rules: [
+//       {
+//         test: /\.css$/i,
+//         use: [
+//           "style-loader",
+//           "css-loader?url=false",
+//           { loader: "postcss-loader", options: { plugins: postCSSPlugins } }
+//         ]
+//       }
+//     ]
+//   }
+// }
+
+module.exports = config
